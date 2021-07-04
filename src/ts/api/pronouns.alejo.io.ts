19a12c2f9b076b5291707058e9b5b3c0b4d44129
Cadence Ember <cadence@disroot.org>
2021-07-04 20:10:03 +1200

    Add custom theme
    - All pronouns are now lowercase
    - If the set is an "expected pair" like he/him, she/her, only show the
      first of the pair
    - Change appearance to have a little more spacing and a soft yet
      distinctive colour.

--- src/ts/api/pronouns.alejo.io.ts
+++ src/ts/api/pronouns.alejo.io.ts
@@ -1,18 +1,21 @@
 import Pronoun, { IPronouns } from "../types/pronouns";
 import { IUser } from "../types/users";
+import { PRONOUN_OVERWRITES } from "../constants/overwrites";
 
 async function get<T = JSON>(endpoint: string): Promise<T> {
 	return await fetch(process.env.BASE_API_URL + endpoint).then(async (res: Response) => {
 		return res.json() as Promise<T>;
 	})
 }
-	
+
 export async function getPronouns(): Promise<IPronouns> {
-	var res: Pronoun[] = await get<Pronoun[]>("pronouns")
+	var res: Pronoun[] = await get<Pronoun[]>("pronouns");
 	var p: IPronouns = {};
 	res.forEach((pronoun: Pronoun) => {
-		p[pronoun.name] = pronoun.display;
+		p[pronoun.name] = pronoun.display.toLowerCase(); // Save all display strings as lowercase, for (ae)sthetics
 	});
+	// Overwrite fetched pronouns with local version where available
+	Object.assign(p, PRONOUN_OVERWRITES);
 	return p;
 }
 
---
+++ src/ts/constants/overwrites.ts
@@ -0,0 +1,10 @@
+import { IPronouns } from "../types/pronouns";
+
+export const PRONOUN_OVERWRITES: IPronouns = {
+	"any": "any",
+	"faefaer": "fae",
+	"hehim": "he",
+	"itits": "it",
+	"sheher": "she",
+	"theythem": "they",
+}
